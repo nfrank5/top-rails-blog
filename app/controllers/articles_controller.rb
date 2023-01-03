d89564b3f0391e47499d5bf6249f8e490bcfd6b8
nfrank5 <nfrankenfeld@gmail.com>
2023-01-03 09:52:11 +0800

    Add updating article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    
+
     if @article.save
       redirect_to @article
     else
@@ -21,10 +21,22 @@ class ArticlesController < ApplicationController
     end
   end
 
-  private
-
-  def article_params
-    params.require(:article).permit(:title, :body)
+  def edit
+    @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
